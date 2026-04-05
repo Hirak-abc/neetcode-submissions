fa16f2495802a61ace2c5d42bffd6b7ca97e418a
Hirak Nath <hiraknath816@gmail.com>
2026-04-05 13:32:25 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(nums, 0, res);
+        return res;
+    }
+
+    void backtrack(int[] nums, int i, List<List<Integer>> res) {
+        if (i == nums.length) {
+            List<Integer> temp = new ArrayList<>();
+            for (int n : nums) temp.add(n);
+            res.add(temp);
+            return;
+        }
+
+        for (int j = i; j < nums.length; j++) {
+            int t = nums[i]; nums[i] = nums[j]; nums[j] = t;
+
+            backtrack(nums, i + 1, res);
+
+            t = nums[i]; nums[i] = nums[j]; nums[j] = t;
+        }
+    }
+}
